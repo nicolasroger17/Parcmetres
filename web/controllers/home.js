22e05b8164d2a58e7e84b727705aa9a1f23b5a40
nicolasroger17 <nicolas.roger@isep.fr>
2014-05-09 16:32:36 +0200

    inscription -> to check

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -1,44 +1,20 @@
 var model = require('../models/home');
 
 module.exports.controller = function(app) {
-
 	app.get('/', function(req, res) {
-  		console.log(req.session);
-		if(req.session.sessionID){
-			res.render('home/home');	
-		}
-		else{
-			res.render('home/connexion');
-			app.post('/connexion', function(req, res) {
-				model.connexion(req, res);
-			});
-		}
+		home(app, req, res);
 	});
 
 	app.get('/home', function(req, res) {
-		if(req.session.sessionID){
-			res.render('home/home');	
-		}
-		else{
-			res.render('home/connexion');
-			app.post('/connexion', function(req, res) {
-				model.connexion(req, res);
-			});
-		}	 	
+		home(app, req, res);
 	});
 
 	app.get('/connexion', function(req, res) {
-		res.render('home/connexion');
-		app.post('/connexion', function(req, res) {
-			model.connexion(req, res);
-		});
+		connexion(app, req, res);
 	});
 
 	app.get('/inscription', function(req, res) {
-		res.render('home/inscription');
-		app.post('/inscription', function(req, res) {
-			model.inscription(req, res);
-		});
+		inscription(app, req, res);
 	});
 
 	app.get('/deconnexion', function(req, res) {
@@ -47,5 +23,28 @@ module.exports.controller = function(app) {
 		  {Location: '/'}
 		);
 		res.end();
+	});
+}
+
+function home(app, req, res){
+	if(req.session.sessionID){
+		res.render('home/home');	
+	}
+	else{
+		connexion(app, req, res);
+	}
+}
+
+function connexion(app, req, res){
+	res.render('home/connexion');
+	app.post('/connexion', function(req, res) {
+		model.connexion(req, res);
+	});
+}
+
+function inscription(app, req, res){
+	res.render('home/inscription');
+	app.post('/inscription', function(req, res) {
+		model.inscription(req, res);
 	});
 }
--- web/models/home.js
+++ web/models/home.js
@@ -11,33 +11,64 @@ var connection = mysql.createConnection({
 connection.connect();
 
 var inscription = function(req, res){
-	var query = connection.query('INSERT INTO users SET ?', req.body, function(err, result) {
-		return err==null;
-	});
-}
-
-var	connexion = function(req, res){
-	var query = connection.query('SELECT id, lastName, firstName FROM users where emailAddress= ?  && password= ?', [req.body.emailAddress, req.body.password], function(err, result){
-		if(err == null){
+	// check if a user with the same mail address exists
+	var query = connection.query('SELECT emailAddress FROM users where emailAddress= ?', [req.body.emailAddress], function(err, result){
+		// if no error
+		if(err==null){
+			// if the mail address is free
 			if(result.length > 0){
-				req.session.sessionID = result[0].id;
-				req.session.lastName = result[0].lastName;
-				req.session.firstName = result[0].firstName;
-				res.writeHead(301,
-				  {Location: '/'}
-				);
-				res.end();
+				var query = connection.query('INSERT INTO users SET ?', req.body, function(err, result) {
+					console.log(err);
+					console.log(result);
+					if(err == null){
+						res.writeHead(301,
+							{Location: '/connexion'}
+						);
+						res.end();
+					}
+					else{
+						res.writeHead(301,
+							{Location: '/inscription'}
+						);
+						res.end();
+					}
+				});
 			}
 			else{
 				res.writeHead(301,
-				  {Location: '/connexion'}
+					{Location: '/inscription'}
 				);
 				res.end();
 			}
 		}
 		else{
 			res.writeHead(301,
-			  {Location: '/connexion'}
+				{Location: '/inscription'}
+			);
+			res.end();
+		}
+}
+
+var	connexion = function(req, res){
+	var query = connection.query('SELECT id, lastName, firstName FROM users where emailAddress= ?  && password= ?', [req.body.emailAddress, req.body.password], function(err, result){
+		if(err == null){
+			if(result.length > 0){
+				req.session.sessionID = result[0].id; req.session.lastName = result[0].lastName; req.session.firstName = result[0].firstName;
+				res.writeHead(301,
+					{Location: '/home'}
+				);
+				res.end();
+			}
+			else{
+				res.writeHead(301,
+					{Location: '/connexion'}
+				);
+				res.end();
+			}
+		}
+		else{
+			res.writeHead(301,
+				{Location: '/connexion'}
 			);
 			res.end();
 		}
